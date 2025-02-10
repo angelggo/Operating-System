f926c55713d0cef96432ac204588a63439140919
angelggo <angelggo200401@gmail.com>
2025-02-10 14:14:15 -0600

    Setting up GUI

---
+++ build/CMakeFiles/fltk_app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fltk_app.dir/main.cpp.o"
+  "CMakeFiles/fltk_app.dir/main.cpp.o.d"
+  "fltk_app"
+  "fltk_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fltk_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
 #include <iostream>
 
-int main() {
-  // program code goes here
-  return 0;
+// Callback function for the button
+void button_callback(Fl_Widget* w, void* data) {
+    std::cout << "Button clicked!" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    Fl_Window window(400, 300, "FLTK Example");  // Create window
+    Fl_Button button(150, 130, 100, 50, "Send");  // Create button
+    button.callback(button_callback);  // Assign callback function
+    window.show(argc, argv);  // Show window
+    return Fl::run();  // Run event loop
 }
