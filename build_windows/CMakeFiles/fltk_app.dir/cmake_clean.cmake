55cda8fcc209bd08381a1c4d234f9853c81173f2
angelggo <angelggo200401@gmail.com>
2025-02-11 22:18:23 -0600

    compiling .exe file from windows machine

---
+++ build_windows/CMakeFiles/fltk_app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fltk_app.dir/functions.cpp.obj"
+  "CMakeFiles/fltk_app.dir/functions.cpp.obj.d"
+  "CMakeFiles/fltk_app.dir/main.cpp.obj"
+  "CMakeFiles/fltk_app.dir/main.cpp.obj.d"
+  "fltk_app.exe"
+  "fltk_app.exe.manifest"
+  "fltk_app.pdb"
+  "libfltk_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fltk_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
